73db7b4857047a81341789cdda9256b5af9d6d67
Abby <Abby@DESKTOP-VKT39EJ>
2020-04-20 20:38:51 -0700

    FlightTest & UserTest

---
+++ src/test/java/cst438/fr/fe/FlightTest.java
@@ -0,0 +1,14 @@
+package cst438.fr.fe;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+public class FlightTest {
+
+}
